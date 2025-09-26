d358d22afb3ce7fae09ee85fcb9e39720c585a36
MallammaHR <mallumanish17@gmail.com>
2025-09-25 22:16:28 -0600

    OHRM-xpath

--- src/xpathINCT/BasicXpath.ts
+++ src/xpathINCT/BasicXpath.ts
@@ -13,19 +13,19 @@ import {chromium,Browser, Page} from'@playwright/test';
 
     let page : Page = await browser.newPage();
     
-    // page.goto('https://naveenautomationlabs.com/opencart/index.php?route=account/login');
+    page.goto('https://naveenautomationlabs.com/opencart/index.php?route=account/login');
 
-    // 1.//xpath with only "id" with single attribute
-    // //syntax=//tagname[@id='value']
-    // await page.locator('//input[@id="input-firstname"]').fill('selenium1234@open.com');
+    1.//xpath with only "id" with single attribute
+    //syntax=//tagname[@id='value']
+    await page.locator('//input[@id="input-firstname"]').fill('selenium1234@open.com');
 
-    // 2.//With mulitple atrributes
-    // //syntax=>//tagname[@an="av" and @an="av" and @an=av]
-    // await page.locator('//input[@id="input-firstname" and @name="firstname" and @placeholder="First Name"]').fill('Alanso');
+    2.//With mulitple atrributes
+    //syntax=>//tagname[@an="av" and @an="av" and @an=av]
+    await page.locator('//input[@id="input-firstname" and @name="firstname" and @placeholder="First Name"]').fill('Alanso');
 
-    // 3.//without tagname
-    // //*[@id="value"]
-    // await page.locator(`//*[id="input-firstname]`).fill('jeeva');//better tio avoide 
+    3.//without tagname
+    //*[@id="value"]
+    await page.locator(`//*[id="input-firstname]`).fill('jeeva');//better tio avoide 
 
     // 4.//text() in the xpath:span,div,para,link,label,anytext
     // //tagname[text()="value"] where text is the existing function in the xpath
@@ -46,24 +46,24 @@ import {chromium,Browser, Page} from'@playwright/test';
      //9.conatins with text() with other attributes
     //a[contains(text(),'Address Book') and contains(@class, 'list-group-item')]
 
-    await page.goto('https://www.orangehrm.com/en/contact-sales/');
-    // 10.preceding-siblibg::
-    let precedInpt: string = await page.locator(`//option[@value='Algeria']/preceding-sibling::option[@value='Afghanistan']`).innerText();
-    console.log( precedInpt);//Afganistan
+    // await page.goto('https://www.orangehrm.com/en/contact-sales/');
+    // // 10.preceding-siblibg::
+    // let precedInpt: string = await page.locator(`//option[@value='Algeria']/preceding-sibling::option[@value='Afghanistan']`).innerText();
+    // console.log( precedInpt);//Afganistan
 
-    //11.following-sibling
-    let folloInpt:string =await page.locator(`//option[@value='Algeria']/following-sibling::option[@value='Angola']`).innerText();
-    console.log(folloInpt);//Angola
+    // //11.following-sibling
+    // let folloInpt:string =await page.locator(`//option[@value='Algeria']/following-sibling::option[@value='Angola']`).innerText();
+    // console.log(folloInpt);//Angola
 
-    //indexing in xpath
-    // (//input[@class='form-control'])[position()=1]
-     await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
-    //(//input[@class='form-control'])[last()]
-     await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
-     //(//input[@class='form-control'])[last()-1]
-     await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
-     //(//input[@class='form-control'])[last()-3]
-     await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
+    // //indexing in xpath
+    // // (//input[@class='form-control'])[position()=1]
+    //  await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
+    // //(//input[@class='form-control'])[last()]
+    //  await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
+    //  //(//input[@class='form-control'])[last()-1]
+    //  await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
+    //  //(//input[@class='form-control'])[last()-3]
+    //  await  page.locator(`(//input[@class='form-control'])[position()=1]`).fill("12344");
 
 
 
---
+++ src/xpathINCT/ohrmXpath.ts
@@ -0,0 +1,51 @@
+import {chromium,Browser, Page} from'@playwright/test';
+ 
+
+///This is not the complete program and i wrote all the X-Path here and what all the best way i write xpath
+//IIFE
+(async () => {
+
+      let browser: Browser = await chromium.launch({
+
+        headless :false,
+        channel :'chrome',
+    });
+
+    let page : Page = await browser.newPage();
+
+     await page.goto('https://www.orangehrm.com/en/contact-sales/');
+
+  
+    //1.id
+     await page.locator(`//input[@id='Form_getForm_FullName']`).fill('mallu');
+    
+     //2.name
+     await page.locator(`//input[@name='Contact']`).fill("roopa@gmail.com");
+    
+     //3.class
+     await page.locator(`//input[@class='email text']`).fill("90837423082364092834");
+
+     //4.parent/child(only the direct child elements) & also we can use //parent//child(direct + indirect child elements)
+    await page.locator(`//select[@id='Form_getForm_Country']`).selectOption({ value: 'Algeria' });
+
+       //5.text() Allow All cokkies
+     await page.locator(`//button[text()='Allow all']`).click();
+
+    //6.other attribute
+    await page.locator(`//input[@placeholder='Company Name']`).fill("ABCSD");
+
+    //7 contains() with attribute
+    await page.locator(`//input[contains(@placeholder,'Job')]`).fill('QA');
+
+    //8.conatins with text()
+     await page.locator(`//select[@id='Form_getForm_NoOfEmployees']`).selectOption({value : '51 - 200'});
+
+     //9. conatins() and  id(without conatins)
+     page.locator(`//textarea[contains(@placeholder,'Your') and  @id='Form_getForm_Comment']`).fill('A good plat foam');
+     
+     //captch box click
+     await page.locator(`//div[@class='recaptcha-checkbox-border']`).check();
+
+     //click on conatct sales
+     await page.locator(`//input[@value='Contact Sales']`).click();
+})();
