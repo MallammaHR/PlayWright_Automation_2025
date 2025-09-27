cedad0859b912cd577cf2f2e9ea4467e9d1c0bfe
MallammaHR <mallumanish17@gmail.com>
2025-09-26 22:49:13 -0600

    total links/img

---
+++ src/totalLinks.ts
@@ -0,0 +1,29 @@
+import {chromium,Browser,Locator, Page} from'@playwright/test';
+ 
+
+///This is not the complete program and i wrote all the X-Path here and what all the best way i write xpath
+//IIFE
+(async () => {
+
+      let browser: Browser = await chromium.launch({
+
+        headless :false,
+        channel :'chrome',
+    });
+
+    let page : Page = await browser.newPage();
+
+     await page.goto('https://www.amazon.co.in');
+
+    //total links on the page where href
+     let totalLinks : string[] = await page.locator(`//a[@href]`).allInnerTexts();//css=a[href]
+     console.log(totalLinks.length);
+
+     
+    //img on the page where alt
+     //total links on the page where href
+     let totalimg: string[] = await page.locator(`img[alt]`).allInnerTexts();//css=img[//@alt]
+     console.log(totalimg.length);
+
+
+})();
